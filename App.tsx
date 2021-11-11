0815fb0ddbdb44b01a2a6f1e2789fb61a7172190
KLEBER HENRIQUE FELIX DOS SANTOS <kleber.hfsantos@senacsp.edu.br>
2021-11-11 11:31:31 -0300

    Fim do projeto

--- App.tsx
+++ App.tsx
@@ -29,70 +29,107 @@ export default function App() {
       </View>
       {/* fim do header =========*/}
 
+      <ScrollView horizontal={false}>
 
-      {/* INICIO DOS STORYS =========*/}
-      <View style={estilo.stories}>
-        <Text style={estilo.titulostories}>
-          Stories
-        </Text>
 
-        <ScrollView horizontal={true} style={estilo.stories}>
+        {/* INICIO DOS STORYS =========*/}
+        <View style={estilo.stories}>
+          <Text style={estilo.titulostories}>
+            Stories
+          </Text>
 
-          <View>
-            <Text style={estilo.blackbox}>Live</Text>
-            <Image source={{ uri: "https://images.unsplash.com/photo-1488426862026-3ee34a7d66df?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjF8fHBlb3BsZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" }} style={estilo.imgstories} />
-            
-            <View style={estilo.txticon}>
-            <Text style={estilo.txt}>Savanna</Text>
-            <Image source={require("./assets/verificado.png")} style={estilo.icon}/>
+          <ScrollView horizontal={true} style={estilo.stories}>
+
+            <View>
+              <Text style={estilo.blackbox}>Live</Text>
+              <Image source={{ uri: "https://images.unsplash.com/photo-1488426862026-3ee34a7d66df?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjF8fHBlb3BsZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" }} style={estilo.imgstories} />
+
+              <View style={estilo.txticon}>
+                <Text style={estilo.txt}>Savanna</Text>
+                <Image source={require("./assets/verificado.png")} style={estilo.icon} />
+              </View>
+
             </View>
-          
+
+            <View>
+              <Text style={estilo.redbox}>Premiere</Text>
+              <Image source={{ uri: "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" }} style={estilo.imgstories} />
+
+              <View style={estilo.txticon}>
+                <Text style={estilo.txt}>Cooper</Text>
+                <Image source={require("./assets/verificado.png")} style={estilo.icon} />
+              </View>
+
+            </View>
+
+            <View>
+              <Text style={estilo.blackbox}>Live</Text>
+              <Image source={{ uri: "https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" }} style={estilo.imgstories} />
+
+              <View style={estilo.txticon}>
+                <Text style={estilo.txt}>Roberto</Text>
+                <Image source={require("./assets/verificado.png")} style={estilo.icon} />
+              </View>
+            </View>
+
+            <View>
+              <Text style={estilo.blackbox}>Live</Text>
+              <Image source={{ uri: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80" }} style={estilo.imgstories} />
+
+              <View style={estilo.txticon}>
+                <Text style={estilo.txt}>Francis</Text>
+                <Image source={require("./assets/verificado.png")} style={estilo.icon} />
+              </View>
+            </View>
+
+            <View>
+              <Text style={estilo.blackbox}>Live</Text>
+              <Image source={{ uri: "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80" }} style={estilo.imgstories} />
+
+              <View style={estilo.txticon}>
+                <Text style={estilo.txt}>Alfred</Text>
+                <Image source={require("./assets/verificado.png")} style={estilo.icon} />
+              </View>
+            </View>
+          </ScrollView>
+
+        </View>
+        {/* FIM DOS STORYS =========*/}
+
+        {/*Inicio do Trending  =========*/}
+        <View style={estilo.trending}>
+          <View style={estilo.cxtitulo}>
+            <Text style={estilo.trendingtitulo}>
+              Trending
+            </Text>
+            <Image source={require("./assets/more.png")} style={estilo.trendingmore} />
+          </View>
+          <View style={estilo.cxtrendingdetalhe}>
+            <View>
+              <Image source={{ uri: "https://magazine.zarpo.com.br/wp-content/uploads/2019/02/A-sensacao-de-saltar-de-paraquedas-1200x900.jpg" }} style={estilo.detalheimg} />
+              <Text style={estilo.detalhelike}>Like</Text>
+            </View>
+
+            <View style={estilo.trendingusuario}>
+              <Image source={{ uri: "https://br.web.img3.acsta.net/c_310_420/pictures/19/08/14/22/33/0632419.jpg" }} style={estilo.trendingusuarioimg} />
+              <View>
+                <Text style={estilo.trendingusuariotitulo}>Lionel Messi</Text>
+                <Text style={estilo.trendingusuariosub}>2 minutos atrás</Text>
+              </View>
+            </View>
+
+
+            <Text style={estilo.trendingobs}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </Text>
           </View>
 
-          <View>
-            <Text style={estilo.redbox}>Premiere</Text>
-            <Image source={{ uri: "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" }} style={estilo.imgstories} />
-          
-            <View style={estilo.txticon}>
-            <Text style={estilo.txt}>Cooper</Text>
-            <Image source={require("./assets/verificado.png")} style={estilo.icon}/>
-            </View>
-          
-          </View>
+        </View>
 
-          <View>
-            <Text style={estilo.blackbox}>Live</Text>
-            <Image source={{ uri: "https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" }} style={estilo.imgstories} />
-          
-            <View style={estilo.txticon}>
-            <Text style={estilo.txt}>Roberto</Text>
-            <Image source={require("./assets/verificado.png")} style={estilo.icon}/>
-            </View>
-          </View>
+        {/* FIm do Trending */}
 
-          <View>
-            <Text style={estilo.blackbox}>Live</Text>
-            <Image source={{ uri: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80" }} style={estilo.imgstories} />
-          
-            <View style={estilo.txticon}>
-            <Text style={estilo.txt}>Francis</Text>
-            <Image source={require("./assets/verificado.png")} style={estilo.icon}/>
-            </View>
-          </View>
-
-          <View>
-            <Text style={estilo.blackbox}>Live</Text>
-            <Image source={{ uri: "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80" }} style={estilo.imgstories} />
-          
-            <View style={estilo.txticon}>
-            <Text style={estilo.txt}>Alfred</Text>
-            <Image source={require("./assets/verificado.png")} style={estilo.icon}/>
-            </View>
-          </View>
-        </ScrollView>
-
-      </View>
-      {/* FIM DOS STORYS =========*/}
+        {/* Todos os elementos devem ficar acima do ScrollView */}
+      </ScrollView>
     </View>
 
 
--- css/Styles.tsx
+++ css/Styles.tsx
@@ -114,6 +114,78 @@ export const estilo = StyleSheet.create({
         width:18,
         height:18,
         resizeMode:'contain'
+    },
+    trending:{
+    },
+    cxtitulo:{
+        flexDirection:'row',
+        alignItems:'center',
+        marginTop:30,
+        marginBottom:30
+    },
+    trendingtitulo:{
+        fontSize:30,
+        color:'#989cac',
+        paddingLeft:10
+    },
+    trendingmore:{
+        width:60,
+        height:20,
+        resizeMode:'contain',
+        marginLeft:'auto',
+        paddingRight:90
+    },
+    cxtrendingdetalhe:{
+        backgroundColor:'#403d40',
+        borderRadius:20,
+        padding:10,
+        width:'90%',
+        marginLeft:'auto',
+        marginRight:'auto'
+    },
+    detalheimg:{
+        width:'100%',
+        height:300,
+        borderRadius:10,
+        resizeMode:'cover'
+    },
+    detalhelike:{
+        backgroundColor:'#ff6',
+        padding:10,
+        borderWidth:5,
+        borderColor:'rgba(255,255,100,0.7)',
+        position:'absolute',
+        zIndex:1000,
+        bottom:0, /*bottom foi utilizado para mandar o link para baixo*/ 
+        right:0, /**Utilizado para mandar o link para a direita */
+        borderBottomRightRadius:10,
+        borderTopLeftRadius:5,
+        textAlign:"center"
+    },
+    trendingusuario:{
+       flexDirection:'row',
+       marginTop:20,
+       marginBottom:10
+    },
+    trendingusuarioimg:{
+        width:50,
+        height:50,
+        borderRadius:20,
+        resizeMode:'cover',
+        marginRight:10
+    },
+    trendingusuariotitulo:{
+        color:'#989cac',
+        fontSize:20,
+        fontWeight:'bold'
+
+    },
+    trendingusuariosub:{
+        color:'#989cac'
+    },
+    trendingobs:{
+        color:'#989cac'
+
     }
 
 })
